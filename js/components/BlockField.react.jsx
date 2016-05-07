37c563b2ca4de89ea340da1c79c2c92283ba79f1
shundroid <ra24483@qb3.so-net.ne.jp>
2016-05-07 12:10:34 +0900

    Storeの変更をViewに反映できるよう実装

--- js/components/BlockField.react.jsx
+++ js/components/BlockField.react.jsx
@@ -1,14 +1,45 @@
 import React from "react";
 
 import BlockStore from "../stores/BlockStore";
+import Block from "./Block.react.jsx";
 import Appender from "./Appender.react.jsx";
 
+function getBlockState() {
+  return {
+    blocks: BlockStore.getAll()
+  }
+}
 export default class BlockField extends React.Component {
+  constructor(prop) {
+    super(prop);
+    this.state = getBlockState();
+  }
+  componentDidMount() {
+    BlockStore.addChangeListener(this._onChange.bind(this));
+  }
+  componentWillUnmount() {
+    BlockStore.removeChangeListener(this._onChange.bind(this));
+  }
   render() {
+    console.log("fuga");
+    let blocks = [];
+    this.state.blocks.forEach(blockDetail => {
+      blocks.push(
+        <Block
+          itemColor={blockDetail.color}
+          icon={blockDetail.icon}
+        />
+      );
+    });
     return (
       <ul id="block-field">
+        {blocks}
         <Appender />
       </ul>
     );
+  }
+  _onChange() {
+    console.log(this.state);
+    this.setState(getBlockState());
   }
 }
